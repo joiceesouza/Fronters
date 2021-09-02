303fc158baceed9f6d21369506ffe5bc5250892b
tamaracosta <tamarasantosc@gmail.com>
2021-09-02 00:12:33 -0300

    eslint routes

--- src/routes.js
+++ src/routes.js
@@ -1,55 +1,32 @@
-
-import {TemplateLogin} from './pages/login/index.js';
-import {TemplateCadastro} from './pages/cadastro/index.js';
-import {TemplatePerfil} from './pages/perfil/index.js';
-import {TemplateRecuperar} from './pages/recuperar/index.js';
-import {TemplateFeed} from './pages/feed/index.js';
+import { TemplateLogin } from './pages/login/index.js';
+import { TemplateCadastro } from './pages/cadastro/index.js';
+import { TemplatePerfil } from './pages/perfil/index.js';
+import { TemplateRecuperar } from './pages/recuperar/index.js';
+import { TemplateFeed } from './pages/feed/index.js';
 
 export const routeRender = () => {
- 
   firebase.auth().onAuthStateChanged((user) => {
-     const rotaAtual = window.location.pathname
-    if(user == null && rotaAtual != "/cadastro" && rotaAtual != "/recuperar" && rotaAtual != "/login") {
-      window.history.pushState({}, null, '/login')
-      const popStateEvent = new PopStateEvent("popstate", {})
-      dispatchEvent(popStateEvent)
+    const rotaAtual = window.location.pathname;
+    if (user == null && rotaAtual !== '/cadastro' && rotaAtual !== '/recuperar' && rotaAtual !== '/login') {
+      window.history.pushState({}, null, '/login');
+      const popStateEvent = new PopStateEvent('popstate', {});
+      dispatchEvent(popStateEvent);
     }
-    
+
     const element = document.querySelector('#root');
     const routes = {
-      '/': TemplateLogin, 
+      '/': TemplateLogin,
       '/login': TemplateLogin,
       '/cadastro': TemplateCadastro,
       '/recuperar': TemplateRecuperar,
-
       '/perfil': TemplatePerfil,
       '/feed': TemplateFeed,
     };
-    
+
     element.innerHTML = '';
     element.appendChild(routes[window.location.pathname]());
-
   });
-  
-   
 };
 
 window.addEventListener('popstate', routeRender);
 window.addEventListener('load', routeRender);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
